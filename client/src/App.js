e165d8222250435b48a9419effc19c67e7b47847
david tenni <davidctenni@gmail.com>
2021-08-23 22:33:05 +1000

    Removed: some comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+//setup heroku
 function App() {
   const [data, setData] = React.useState(null);
 
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Server Test 1" });
+  res.json({ message: "Local Test" });
 });
 
 app.listen(PORT, () => {
